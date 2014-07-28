1b0a8e1b445993a8e56a3975c01beb3cef90d7d5
cyberabis <babishek@gmail.com>
2014-07-28 12:22:46 +0530

    Initialized MVP Project

---
+++ mvp/logbase-quick/src/main/java/io/logbase/App.java
@@ -0,0 +1,13 @@
+package io.logbase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
