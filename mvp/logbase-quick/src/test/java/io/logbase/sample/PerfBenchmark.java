b89cf058022715424d0590a30b3ea9e41ba00297
cyberabis <babishek@gmail.com>
2014-08-24 21:07:44 +0530

    Read benchmarking

--- mvp/logbase-quick/src/test/java/io/logbase/sample/PerfBenchmark.java
+++ mvp/logbase-quick/src/test/java/io/logbase/sample/PerfBenchmark.java
@@ -1,8 +1,6 @@
 package io.logbase.sample;
 
 import bb.util.Benchmark;
-
-import io.logbase.collections.BatchList;
 import io.logbase.collections.impl.IntegerLinkedArrayList;
 import io.logbase.consumer.EventConsumer;
 import io.logbase.consumer.impl.TwitterFileConsumer;
@@ -11,6 +9,9 @@ import io.logbase.exceptions.ConsumerInitException;
 import io.logbase.node.Node;
 import io.logbase.node.NodeConnector;
 import io.logbase.node.impl.SimpleRealtimeNodeConnector;
+import io.logbase.table.TableIterator;
+import io.logbase.utils.InFilter;
+import io.logbase.view.View;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -18,111 +19,161 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.Callable;
 
-
 /**
  * Created by kousik on 06/08/14.
  */
 
 /**
- * To benchmark your code, add the appropriate subroutine to benchmark here
- * and associate it to an enum.
+ * To benchmark your code, add the appropriate subroutine to benchmark here and
+ * associate it to an enum.
  */
 public class PerfBenchmark implements Callable<Integer> {
 
-    public enum ListType {
-        INSERT_LINKEDLIST,
-        INSERT_ARRAYLIST,
-        INSERT_INTLINKARRLIST,
-        TWITTERFILEWRITE};
+  public enum ListType {
+    INSERT_LINKEDLIST, INSERT_ARRAYLIST, INSERT_INTLINKARRLIST, TWITTERFILEWRITE, READ_LINKEDLIST, READ_INTLINKARRLIST, READ_ARRAYLIST, TWITTERFILEREAD
+  };
 
-    static int testSize = 10 * 1000 * 1000;
-    static ListType listType;
+  static int testSize = 10 * 1000;
+  static ListType listType;
+  static List<Integer> linkedList = new LinkedList<Integer>();
+  static IntegerLinkedArrayList intList = new IntegerLinkedArrayList(64 * 1024);
+  static List<Integer> arrayList = new ArrayList<Integer>(testSize + 1);
+  static NodeConnector nodeConnector = new SimpleRealtimeNodeConnector();
+  static Node node = nodeConnector.connect();
 
-    public PerfBenchmark (ListType listType) {
-        this.listType = listType;
+  public PerfBenchmark(ListType listType) {
+    this.listType = listType;
+  }
+
+  /*
+   * The Benchmark library executes the this call subroutine.
+   */
+
+  public Integer call() {
+    switch (this.listType) {
+    case INSERT_INTLINKARRLIST:
+      // BaseList<Integer> intList = new IntegerLinkedArrayList(64 * 1024);
+      // insert(intList);
+      break;
+    case INSERT_LINKEDLIST:
+      // List<Integer> linkedList = new LinkedList<Integer>();
+      insert(linkedList);
+      break;
+    case INSERT_ARRAYLIST:
+      // List<Integer> arrayList = new ArrayList<Integer>(testSize + 1);
+      insert(arrayList);
+      break;
+    case TWITTERFILEWRITE:
+      consumeTwitterFile();
+      break;
+    case READ_LINKEDLIST:
+      // List<Integer> linkedList2 = new LinkedList<Integer>();
+      // insert(linkedList2);
+      read(linkedList);
+      break;
+    case READ_INTLINKARRLIST:
+      // List<Integer> intList2 = new IntegerLinkedArrayList(64 * 1024);
+      // insert(intList2);
+      read(intList);
+      break;
+    case READ_ARRAYLIST:
+      read(arrayList);
+      break;
+    case TWITTERFILEREAD:
+      readTwitterFile();
+      break;
+
     }
+    return 1;
+  }
+
+  private void readTwitterFile() {
+    // Free up other memory
+    linkedList = null;
+    intList = null;
+    arrayList = null;
+
+    View view = node.getReader().getViewFactory()
+        .createView(new InFilter("Twitter"));
+    TableIterator tableIterator = view.getIterator();
+    while (tableIterator.hasNext()) {
+      tableIterator.next();
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Callable<Integer> task;
+    Benchmark.Params params = new Benchmark.Params();
 
     /*
-     * The Benchmark library executes the this call subroutine.
+     * Unset this if you don't want any feedback on what the benchmark routine
+     * is doing.
      */
+    params.setConsoleFeedback(false);
+    params.setNumberMeasurements(1);
 
-    public Integer call() {
-        switch (this.listType) {
-            case INSERT_INTLINKARRLIST:
-                BatchList<Integer> intList = new IntegerLinkedArrayList(64 * 1024);
-                insert(intList);
-                break;
-            case INSERT_LINKEDLIST:
-                List<Integer> linkedList = new LinkedList<Integer>();
-                insert(linkedList);
-                break;
-            case INSERT_ARRAYLIST:
-                List<Integer> arrayList = new ArrayList<Integer>(testSize + 1);
-                insert(arrayList);
-                break;
-            case TWITTERFILEWRITE:
-                consumeTwitterFile();
-                break;
-        }
-        return 1;
-    }
-
-    public static void main(String[] args) throws Exception {
-        Callable<Integer> task;
-        Benchmark.Params params = new Benchmark.Params();
-
-        /*
-         * Unset this if you don't want any feedback on what the benchmark
-         * routine is doing.
-         */
-        params.setConsoleFeedback(true);
-        params.setNumberMeasurements(5);
-
-        /*
-         * Set this if you only need to measure CPU time
-         */
-        params.setMeasureCpuTime(false);
-
-        for (ListType listType : ListType.values()) {
-            task = new PerfBenchmark(listType);
-            System.out.println(listType.toString() + " took " + new Benchmark(task, params).getStats() + "\n");
-        }
-    }
-
-    /**
-     * Benchmarking subroutines go here
+    /*
+     * Set this if you only need to measure CPU time
      */
+    params.setMeasureCpuTime(false);
 
-    private static void insert(BatchList<Integer> list) {
-        for (int i = 0; i < testSize; i++) {
-            list.add(i);
-        }
+    for (ListType listType : ListType.values()) {
+      task = new PerfBenchmark(listType);
+      System.out.println(listType.toString() + " took "
+          + new Benchmark(task, params).getStats() + "\n");
     }
+  }
 
-    private static void insert(List<Integer> list) {
-        for (int i = 0; i < testSize; i++) {
-            list.add(i);
-        }
+  /**
+   * Benchmarking subroutines go here
+   */
+
+  private static void insert(IntegerLinkedArrayList list) {
+    for (int i = 0; i < testSize; i++) {
+      list.add(i);
     }
+  }
 
-    private static void consumeTwitterFile() {
-
-        NodeConnector nodeConnector = new SimpleRealtimeNodeConnector();
-        Node node = nodeConnector.connect();
-
-        String fileName = "twitter_events_mini.dat";
-        URL url = ClassLoader.getSystemResource(fileName);
-        EventConsumer consumer = new TwitterFileConsumer(url.getFile());
-
-        try {
-            consumer.init(node.getWriter("Twitter", JSONEvent.class));
-        } catch (ConsumerInitException e) {
-            System.exit(-1);
-        }
-        /*
-         * Consumer, reads every line from the twitter file and
-         * inserts it to the table it creates.
-         */
-        consumer.start();
+  private static void insert(List<Integer> list) {
+    for (int i = 0; i < testSize; i++) {
+      list.add(i);
     }
+  }
+
+  private static void consumeTwitterFile() {
+
+    String fileName = "twitter_events_mini.dat";
+    URL url = ClassLoader.getSystemResource(fileName);
+    EventConsumer consumer = new TwitterFileConsumer(url.getFile());
+
+    try {
+      consumer.init(node.getWriter("Twitter", JSONEvent.class));
+    } catch (ConsumerInitException e) {
+      System.exit(-1);
+    }
+    /*
+     * Consumer, reads every line from the twitter file and inserts it to the
+     * table it creates.
+     */
+    consumer.start();
+  }
+
+  public static void read(List<Integer> linkedList2) {
+    // for (int i = 0; i < testSize; i++) {
+    // linkedList2.add(i);
+    // }
+    for (int i = 0; i < testSize; i++) {
+      linkedList2.get(i);
+    }
+  }
+
+  public static void read(IntegerLinkedArrayList intList2) {
+    // for (int i = 0; i < testSize; i++) {
+    // intList2.add(i);
+    // }
+    for (int i = 0; i < testSize; i++) {
+      intList2.get(i);
+    }
+  }
+
 }
