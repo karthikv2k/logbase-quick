4f566798ee32351e8ae5991d97057b8e8e9d91ba
cyberabis <babishek@gmail.com>
2014-10-08 21:19:03 +0530

    API code in progress

--- mvp/logbase-api/app/controllers/Application.java
+++ mvp/logbase-api/app/controllers/Application.java
@@ -1,16 +1,46 @@
 package controllers;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import io.logbase.node.Node;
+import io.logbase.node.NodeConnector;
+import io.logbase.node.impl.SimpleRealtimeNodeConnector;
+import io.logbase.querying.optiq.LBSchema;
+import io.logbase.querying.optiq.QueryExecutor;
+import io.logbase.utils.InFilter;
+import io.logbase.view.View;
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
 
   public static Result search(String args) {
-    // TODO
+    NodeConnector nodeConnector = new SimpleRealtimeNodeConnector();
+    Node node = nodeConnector.connect();
+    //TODO change the filter condition.
+    View view = node.getReader().getViewFactory()
+        .createView(new InFilter("Twitter"));
+    LBSchema lbSchema = new LBSchema("TEST");
+    lbSchema.addAsSmartTable("TWITTER", view);
+    QueryExecutor queryExec = new QueryExecutor(lbSchema);
+    String sql = "SELECT \"text.String\", \"source.String\" "
+        + " from \"TEST\".\"TWITTER\" where (\"id_str.String\" = '461506965680951296') and (\"id.Double\" = 461506965680951296 or \"text.String\" = '@smiley_bieber15 nxuq')";
+    int resultCount = 0;
+    try {
+      ResultSet results = queryExec.execute(sql);
+      while (results.next()) {
+        resultCount++;
+        // logger.debug("Text is: " + results.getString("text.String")
+        // + " Source is: " + results.getString("source.String"));
+      }
+    } catch (SQLException e) {
+      Logger.error("Error while executing optiq query: " + sql);
+    }
+    Logger.info("Result count: " + resultCount);
 
-    return ok();
+    return ok("You said: " + args);
   }
 
 }
