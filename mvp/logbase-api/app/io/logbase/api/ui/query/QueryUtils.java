8470e9494a72b3f51dcc8a5045261155d94896a8
cyberabis <babishek@gmail.com>
2014-10-16 23:20:46 +0530

    Query API updates

--- mvp/logbase-api/app/io/logbase/api/ui/query/QueryUtils.java
+++ mvp/logbase-api/app/io/logbase/api/ui/query/QueryUtils.java
@@ -1,11 +1,19 @@
 package io.logbase.api.ui.query;
 
+import io.logbase.node.Node;
+import io.logbase.node.NodeConnector;
+import io.logbase.node.impl.SimpleRealtimeNodeConnector;
+
 public class QueryUtils {
 
+  private static NodeConnector nodeConnector = new SimpleRealtimeNodeConnector();
+  private static Node node = nodeConnector.connect();
+  private static int reqid = 0;
   public static int postQuery(String args, long from, long to, int dataset,
       String source) {
-    int reqid = 0;
     // TODO
+    reqid++;
+    // Store request somewhere
 
     return reqid;
   }
