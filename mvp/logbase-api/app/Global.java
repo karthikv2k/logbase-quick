9154fbac48428be850bc4be7641d05f2599d222f
cyberabis <babishek@gmail.com>
2014-10-28 16:58:38 +0530

    Removing consumer on start as upload api is active

--- mvp/logbase-api/app/Global.java
+++ mvp/logbase-api/app/Global.java
@@ -16,27 +16,6 @@ import play.mvc.Result;
 public class Global extends GlobalSettings {
 
   public void onStart(Application app) {
-
-    Logger.info("Application is starting...");
-    // Step 1: Create a local Realtime Node
-    NodeConnector nodeConnector = new SimpleRealtimeNodeConnector();
-    Node node = nodeConnector.connect();
-
-    // TODO This should be removed and be a load file API
-    // Step 2: Configure and start a File consumer
-    String fileName = "twitter_events_mini.dat";
-    // URL url = ClassLoader.getSystemResource(fileName);
-    URL url = this.getClass().getResource(fileName);
-    Logger.info("File URL: " + url.getPath());
-    EventConsumer consumer = new TwitterFileConsumer(url.getFile());
-
-    try {
-      consumer.init(node.getWriter("Twitter", JSONEvent.class));
-    } catch (ConsumerInitException e) {
-      Logger.error("Consumer init error: " + e.getMessage());
-      System.exit(-1);
-    }
-    consumer.start();
     Logger.info("Application has started");
   }
 
